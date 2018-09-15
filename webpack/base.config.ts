44a795f7f0441304cb15d59af213dc701b116fe2
Giorgio Garasto <giorgio@garasto.it>
2018-09-15 14:34:10 +0200

    chore: Make webpack auto resolve .ejs files

--- webpack/base.config.ts
+++ webpack/base.config.ts
@@ -24,7 +24,7 @@ const config: webpack.Configuration = {
     globalObject: 'self',
   },
   resolve: {
-    extensions: ['.ts', '.js', '.scss', '.css'],
+    extensions: ['.ts', '.js', '.scss', '.css', '.ejs'],
     alias: {
       '~': './src',
     },
