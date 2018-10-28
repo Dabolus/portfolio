cae6d96aafac3198ab333e725a6505e1459bc57c
Giorgio Garasto <giorgio@garasto.it>
2018-10-28 09:26:21 +0100

    fix: Wrong SW navigation route

--- src/sw.js
+++ src/sw.js
@@ -4,5 +4,5 @@ workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
-workbox.routing.registerNavigationRoute('/index.html');
+workbox.routing.registerNavigationRoute('/');
 // workbox.routing.registerRoute(/scripts\/[0-9]+\.js/, workbox.strategies.staleWhileRevalidate(), 'GET');
