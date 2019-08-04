87a0c7a91ef67c6a7748fc55cd5138da73d6e9a1
Giorgio Garasto <giorgio@garasto.it>
2019-08-04 18:27:31 +0200

    chore: Format code using Prettier + ESLint

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,14 +3,15 @@ import Typed from 'typed.js';
 
 const pages: { [key: string]: string } = {
   'about-me': 'About me',
-  'certifications': 'Certifications',
-  'contacts': 'Contacts',
-  'works': 'Works',
+  certifications: 'Certifications',
+  contacts: 'Contacts',
+  works: 'Works',
 };
-const dataPromise = fetch('/api').then((res) => res.json());
+const dataPromise = fetch('/api').then(res => res.json());
 
 const smoothScroll = (val?: number) => {
-  const scrollingElement = document.scrollingElement || document.documentElement;
+  const scrollingElement =
+    document.scrollingElement || document.documentElement;
   const y = typeof val === 'undefined' ? scrollingElement.scrollHeight : val;
   scrollingElement.scroll({
     behavior: 'smooth',
@@ -22,17 +23,26 @@ const smoothScroll = (val?: number) => {
 window.addEventListener('load', () => {
   const main: HTMLElement = document.querySelector('main');
   const pageTitle: HTMLElement = document.getElementById('page-title');
-  const fragments: NodeListOf<HTMLElement> = document.querySelectorAll('#page-container > .page');
-  const menuLinks: NodeListOf<HTMLElement> = document.querySelectorAll('#menu > a');
+  const fragments: NodeListOf<HTMLElement> = document.querySelectorAll(
+    '#page-container > .page',
+  );
+  const menuLinks: NodeListOf<HTMLElement> = document.querySelectorAll(
+    '#menu > a',
+  );
   const since2004Link: HTMLElement = document.getElementById('since-2004-link');
-  const homeLinks: HTMLElement[] = [...Array.prototype.slice.call(menuLinks), since2004Link];
-  const homeSections: NodeListOf<HTMLElement> = document.querySelectorAll('#i-am, #menu');
+  const homeLinks: HTMLElement[] = [
+    ...Array.prototype.slice.call(menuLinks),
+    since2004Link,
+  ];
+  const homeSections: NodeListOf<HTMLElement> = document.querySelectorAll(
+    '#i-am, #menu',
+  );
   const backLink: HTMLAnchorElement = document.querySelector('#back-link');
   // Set up scrolling to the right position when navigating with tabs for accessibility
-  since2004Link
-    .addEventListener('focus', () => smoothScroll(0));
-  menuLinks.forEach((link) =>
-    link.addEventListener('focus', () => smoothScroll()));
+  since2004Link.addEventListener('focus', () => smoothScroll(0));
+  menuLinks.forEach(link =>
+    link.addEventListener('focus', () => smoothScroll()),
+  );
   const age = document.getElementById('age');
   const dob = new Date('1997-09-01T23:20').getTime();
 
@@ -54,12 +64,19 @@ window.addEventListener('load', () => {
     }
   };
 
-  dataPromise.then((data) => {
-    document.getElementById('my-summary').innerHTML = data.summary;
-    document.getElementById('job-title').textContent = data.job.title;
-    document.getElementById('company-name').textContent = data.job.company.name;
-    document.getElementById('company-size').textContent = data.job.company.size;
-  }).catch((e) => document.body.innerHTML = `<h1>Unexpected fatal error!</h1><pre>${e}</pre>`);
+  dataPromise
+    .then(data => {
+      document.getElementById('my-summary').innerHTML = data.summary;
+      document.getElementById('job-title').textContent = data.job.title;
+      document.getElementById('company-name').textContent =
+        data.job.company.name;
+      document.getElementById('company-size').textContent =
+        data.job.company.size;
+    })
+    .catch(
+      e =>
+        (document.body.innerHTML = `<h1>Unexpected fatal error!</h1><pre>${e}</pre>`),
+    );
 
   new Typed('#typed', {
     backDelay: 2000,
@@ -82,29 +99,28 @@ window.addEventListener('load', () => {
     if (path === '') {
       updateMetadata({
         description:
-          'Hi! I\'m Giorgio, a Software Engineer trying to make the web a better place since 2004.',
+          "Hi! I'm Giorgio, a Software Engineer trying to make the web a better place since 2004.",
         image: 'images/propic.jpg',
         title: 'Giorgio Garasto',
       });
       document.title = 'Giorgio Garasto';
       document.body.className = '';
       main.className = '';
-      homeLinks.forEach((link) =>
-        link.setAttribute('tabindex', '0'));
-      homeSections.forEach((section) =>
-        section.removeAttribute('aria-hidden'));
-      const oldElement: HTMLElement =
-        document.querySelector(`#page-container > .page[data-page="${oldPath}"`);
+      homeLinks.forEach(link => link.setAttribute('tabindex', '0'));
+      homeSections.forEach(section => section.removeAttribute('aria-hidden'));
+      const oldElement: HTMLElement = document.querySelector(
+        `#page-container > .page[data-page="${oldPath}"`,
+      );
       if (oldElement) {
         oldElement.focus();
       }
       stopAgeAnimation();
-      setTimeout(() => main.hidden = true, 600);
+      setTimeout(() => (main.hidden = true), 600);
     } else {
       updateMetadata({
         // TODO: write a per-page description
         description:
-          'Hi! I\'m Giorgio, a Software Engineer trying to make the web a better place since 2004.',
+          "Hi! I'm Giorgio, a Software Engineer trying to make the web a better place since 2004.",
         // TODO: maybe also an image
         image: 'images/propic.jpg',
         title: `${pages[path]} - Giorgio Garasto`,
@@ -113,17 +129,20 @@ window.addEventListener('load', () => {
       main.hidden = false;
       setTimeout(() => {
         pageTitle.textContent = pages[path];
-        fragments.forEach((fragment) =>
-          fragment.className = fragment.dataset.page === path ? 'page active' : 'page');
+        fragments.forEach(
+          fragment =>
+            (fragment.className =
+              fragment.dataset.page === path ? 'page active' : 'page'),
+        );
         document.body.className = 'blocked';
         main.className = 'active';
         backLink.focus();
       }, 50);
       setTimeout(() => {
-        homeLinks.forEach((link) =>
-          link.setAttribute('tabindex', '-1'));
-        homeSections.forEach((section) =>
-          section.setAttribute('aria-hidden', 'true'));
+        homeLinks.forEach(link => link.setAttribute('tabindex', '-1'));
+        homeSections.forEach(section =>
+          section.setAttribute('aria-hidden', 'true'),
+        );
         if (path === 'about-me') {
           startAgeAnimation();
         }
