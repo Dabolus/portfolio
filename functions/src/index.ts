6eb5a35668a0ca672835142f709c2780188edc22
Giorgio Garasto <giorgio@garasto.it>
2019-08-12 21:00:02 +0200

    feat: Implement projects list endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,21 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import express from 'express';
 
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const projectsCollection = db.collection('projects');
+
 const app = express();
 
-app.get('/api/test', (req, res) => {
-  res.json({ hello: 'world' });
+app.get('/api/projects', async (_, res) => {
+  const projects = await projectsCollection.get();
+  res.json(
+    projects.docs.map(snapshot => ({
+      id: snapshot.id,
+      ...snapshot.data(),
+    })),
+  );
 });
 
 export const api = functions.https.onRequest(app);
