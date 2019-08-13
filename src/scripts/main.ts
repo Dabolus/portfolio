92c4182945110c327bfa6d3c8e840e6703d227c0
Giorgio Garasto <giorgio@garasto.it>
2019-08-13 19:06:22 +0200

    feat: Implement typing animation

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+import Typed from 'typed.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Configure typing animation
+  new Typed('#typed', {
+    backDelay: 2000,
+    backSpeed: 30,
+    loop: true,
+    showCursor: false,
+    smartBackspace: false,
+    startDelay: 0,
+    stringsElement: '#strings',
+    typeSpeed: 90,
+  });
+});
