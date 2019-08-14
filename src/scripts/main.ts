192f44b84e1ada21d3a04dccdfc028feeebcab97
Giorgio Garasto <giorgio@garasto.it>
2019-08-14 20:30:50 +0200

    feat: Start setting up routing

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,33 @@
+import { installRouter, updateMetadata } from 'pwa-helpers';
 import Typed from 'typed.js/src/typed';
+
+const pages: {
+  [key: string]: {
+    title?: string;
+    description: string;
+  };
+} = {
+  home: {
+    description:
+      "Hi! I'm Giorgio, a Software Engineer trying to make the web a better place since 2004.",
+  },
+  'about-me': {
+    title: 'About me',
+    description: '',
+  },
+  certifications: {
+    title: 'Certifications',
+    description: '',
+  },
+  contacts: {
+    title: 'Contacts',
+    description: '',
+  },
+  works: {
+    title: 'Works',
+    description: '',
+  },
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   // Configure typing animation
@@ -11,5 +40,21 @@ document.addEventListener('DOMContentLoaded', () => {
     startDelay: 0,
     stringsElement: '#strings',
     typeSpeed: 90,
+  });
+
+  // Configure routing
+  installRouter(({ pathname }) => {
+    let path = pathname.substring(1) || 'home';
+    if (!Object.keys(pages).includes(path)) {
+      window.history.replaceState({}, '', '/');
+      path = 'home';
+    }
+    const { title, description } = pages[path];
+    updateMetadata({
+      title: title ? `${title} - Giorgio Garasto` : 'Giorgio Garasto',
+      description,
+      image: 'images/propic.jpg',
+      imageAlt: 'Giorgio Garasto',
+    });
   });
 });
