bbaba51947dced919dd4eced857d9e844378d60d
Giorgio Garasto <giorgio@garasto.it>
2019-08-23 07:57:19 +0200

    feat: Implement client side routing

--- src/scripts/routing.ts
+++ src/scripts/routing.ts
@@ -1,35 +1,45 @@
 import { installRouter, updateMetadata } from 'pwa-helpers';
 
-const pages: {
-  [key: string]: {
-    title?: string;
-    description: string;
-  };
-} = {
-  home: {
-    description:
-      "Hi! I'm Giorgio, a Software Engineer trying to make the web a better place since 2004.",
-  },
-  about: {
-    title: 'About me',
-    description: 'Curious about who I am? Get to know me and what I do.',
-  },
-  certifications: {
-    title: 'Certifications',
-    description: 'Check out my certifications and awards.',
-  },
-  contacts: {
-    title: 'Contacts',
-    description:
-      'Interested in what I do and/or have some questions? Contact me!',
-  },
-  projects: {
-    title: 'Projects',
-    description: 'Check out the list of the projects I built and maintain.',
-  },
-};
-
 export const configureRouting = () => {
+  const pages: {
+    [key: string]: {
+      title?: string;
+      description: string;
+    };
+  } = {
+    home: {
+      description:
+        "Hi! I'm Giorgio, a Software Engineer trying to make the web a better place since 2004.",
+    },
+    about: {
+      title: 'About me',
+      description: 'Curious about who I am? Get to know me and what I do.',
+    },
+    certifications: {
+      title: 'Certifications',
+      description: 'Check out my certifications and awards.',
+    },
+    contacts: {
+      title: 'Contacts',
+      description:
+        'Interested in what I do and/or have some questions? Contact me!',
+    },
+    projects: {
+      title: 'Projects',
+      description: 'Check out the list of the projects I built and maintain.',
+    },
+  };
+
+  const home = document.querySelector<HTMLElement>('#home');
+  const page = document.querySelector<HTMLElement>('#page');
+  const pageTitle = document.querySelector<HTMLHeadingElement>('#title');
+  const about = document.querySelector<HTMLDivElement>('#about');
+  const projects = document.querySelector<HTMLDivElement>('#projects');
+  const certifications = document.querySelector<HTMLDivElement>(
+    '#certifications',
+  );
+  const contacts = document.querySelector<HTMLDivElement>('#contacts');
+
   // Configure routing
   installRouter(({ pathname }) => {
     let path = pathname.substring(1) || 'home';
@@ -42,5 +52,22 @@ export const configureRouting = () => {
       title: title ? `${title} - Giorgio Garasto` : 'Giorgio Garasto',
       description,
     });
+
+    if (path === 'home') {
+      home.hidden = false;
+      page.hidden = true;
+      about.hidden = true;
+      projects.hidden = true;
+      certifications.hidden = true;
+      contacts.hidden = true;
+    } else {
+      pageTitle.textContent = title;
+      home.hidden = true;
+      page.hidden = false;
+      about.hidden = path !== 'about';
+      projects.hidden = path !== 'projects';
+      certifications.hidden = path !== 'certifications';
+      contacts.hidden = path !== 'contacts';
+    }
   });
 };
