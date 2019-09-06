c98801ca9d306a504d9b3af7cb69cb7ec9e3720f
Giorgio Garasto <giorgio@garasto.it>
2019-09-06 22:31:16 +0200

    fix: Cloudflare lib is broken, so let's just manually fetch

--- scripts/cloudflare.js
+++ scripts/cloudflare.js
@@ -1,12 +1,18 @@
-const cloudflare = require('cloudflare');
+const fetch = require('node-fetch');
 
 const {
   env: { CLOUDFLARE_API_TOKEN: token, CLOUDFLARE_ZONE_ID: zoneId },
 } = process;
 
-const cf = cloudflare({ token });
-
 module.exports = () =>
-  cf.zones.purgeCache(zoneId, {
-    purge_everything: true,
+  fetch(`https://api.cloudflare.com/client/v4/zones/${zoneId}/purge_cache`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      purge_everything: true,
+    }),
   });
