b3547d0b983e69016fa9866306b62ffd698e2711
Giorgio Garasto <giorgio@garasto.it>
2019-09-30 07:05:46 +0200

    feat: Add function that gets certifications

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 export { processProjectImage } from './image';
 export { getProjects } from './projects';
+export { getCertifications } from './certifications';
 export { getSkills } from './skills';
 export { ssr } from './ssr';
