4d688a13d62ecc2e03d3775e85c8703e089dbb27
Giorgio Garasto <giorgio@garasto.me>
2021-04-23 18:59:51 +0200

    refactor: statically compute skills section

--- pwa/scripts/helpers/data/skills.ts
+++ pwa/scripts/helpers/data/skills.ts
@@ -3,18 +3,18 @@ import { readConfigFile } from './utils';
 
 export interface LanguageSizeData {
   readonly name: string;
-  readonly size: string;
+  readonly size: number;
   readonly color: string;
 }
 
 export interface GitHubData {
-  readonly total: string;
+  readonly total: number;
   readonly languages: readonly LanguageSizeData[];
 }
 
 export interface SkillData {
-  readonly score: number;
-  readonly color?: string;
+  readonly score?: number;
+  readonly color: string;
 }
 
 export interface SkillsData {
@@ -23,18 +23,11 @@ export interface SkillsData {
   readonly soft: Record<string, SkillData>;
 }
 
-export interface ParsedSkill extends Omit<SkillData, 'color'> {
-  readonly name: string;
-  readonly color: string;
-}
-
 export interface ParsedSkills {
-  readonly bytes: GitHubData;
-  readonly skills: {
-    readonly coding: readonly ParsedSkill[];
-    readonly music: readonly ParsedSkill[];
-    readonly soft: readonly ParsedSkill[];
-  };
+  readonly codeSizeChart: string;
+  readonly codingSkillsChart: string;
+  readonly musicSkillsChart: string;
+  readonly softSkillsChart: string;
 }
 
 const prettifySize = (bytes: number) => {
@@ -106,7 +99,7 @@ const getGithubData = async (
   );
 
   return {
-    total: prettifySize(total),
+    total,
     languages: Object.entries(languages).reduce(
       (languageArr: any, [name, { color, size }]: [string, any]) => {
         const index = languageArr.findIndex(
@@ -120,7 +113,7 @@ const getGithubData = async (
           ...firstSlice,
           {
             name,
-            size: prettifySize(size),
+            size,
             color:
               color ||
               codingSkillsData[name]?.color ||
@@ -134,29 +127,111 @@ const getGithubData = async (
   };
 };
 
-const parseSkills = (
-  skillsData: Record<string, SkillData>,
-): readonly ParsedSkill[] =>
-  Object.entries(skillsData)
-    .filter(([, { color }]) => !!color)
-    .map(
-      ([name, data]) =>
-        ({
-          name,
-          ...data,
-        } as ParsedSkill),
-    );
+const computePie = (
+  data: readonly LanguageSizeData[],
+  totalSize: number,
+  size = 100,
+) => {
+  let R = 0;
+  const L = size / 2;
+
+  return `
+    <svg viewBox="0 0 ${size} ${size}">
+      ${data.reduce((sectors, { name, size, color }) => {
+        const a = 360 * (size / totalSize);
+        const aCalc = a > 180 ? 360 - a : a;
+        const aRad = (aCalc * Math.PI) / 180;
+        const z = Math.sqrt(2 * L * L - 2 * L * L * Math.cos(aRad));
+        const x =
+          aCalc <= 90
+            ? L * Math.sin(aRad)
+            : L * Math.sin(((180 - aCalc) * Math.PI) / 180);
+        const y = Math.sqrt(Math.max(0, z * z - x * x));
+        const Y = y;
+        const [X, arcSweep] = a <= 180 ? [L + x, 0] : [L - x, 1];
+
+        const path = `
+          <path
+            class="sector"
+            aria-label="${name}"
+            data-size="${prettifySize(size)}"
+            fill="${color || 'var(--theme-card-background)'}"
+            d="M${L},${L} L${L},0 A${L},${L} 0 ${arcSweep},1 ${X}, ${Y} z"
+            transform="rotate(${R}, ${L}, ${L})"
+          />
+        `;
+
+        R += a;
+
+        return `
+          ${sectors}
+          ${path}
+        `;
+      }, '')}
+      <circle
+        cx="${size / 2}"
+        cy="${size / 2}"
+        r="${size / 3}"
+        fill="var(--theme-content-background)"
+      />
+      <text
+        x="${size / 2}"
+        y="${size / 2}"
+        text-anchor="middle"
+        dominant-baseline="middle"
+        fill="var(--theme-color)"
+        font-size="6px"
+      >
+        <tspan id="language-name" x="${
+          size / 2
+        }" dy="-0.5em">Tap or hover a language</tspan>
+        <tspan id="language-size" x="${
+          size / 2
+        }" dy="1.5em">to see its stats.</tspan>
+      </text>
+    </svg>
+  `;
+};
+
+const computeLineChart = (data: Record<string, SkillData>, size = 100) => {
+  const entries = Object.entries(data).filter(([, { score }]) => !!score);
+
+  return `
+    <svg viewBox="0 0 ${size} ${entries.length * 7 - 2}">
+      ${entries.reduce(
+        (lineChart, [name, { score, color }], index) => `
+          ${lineChart}
+          <text
+            x="0"
+            y="${index * 7 + 2.5}"
+            dominant-baseline="middle"
+            fill="var(--theme-color)"
+            font-size="4px"
+          >
+            ${name}
+          </text>
+          <rect
+            fill="${color || 'var(--theme-card-background)'}"
+            width="${(size / 3) * 2 * score}"
+            height="5"
+            x="${size / 3}"
+            y="${index * 7}"
+          />
+      `,
+        '',
+      )}
+    </svg>
+  `;
+};
 
 export const getSkills = async (): Promise<ParsedSkills> => {
   const skillsData = (await readConfigFile('skills')) as SkillsData;
-  const bytes = await getGithubData(skillsData.coding);
+  const { languages, total } = await getGithubData(skillsData.coding);
 
   return {
-    skills: {
-      coding: parseSkills(skillsData.coding),
-      music: parseSkills(skillsData.music),
-      soft: parseSkills(skillsData.soft),
-    },
-    bytes,
+    codeSizeChart: computePie(languages, total),
+    codingSkillsChart: computeLineChart(skillsData.coding),
+    musicSkillsChart: computeLineChart(skillsData.music),
+    softSkillsChart: computeLineChart(skillsData.soft),
   };
 };
