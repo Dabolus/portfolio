3d299c35fa2b3282220116f228cdb3cd9f1e8bbe
Giorgio Garasto <giorgio@garasto.me>
2021-10-10 14:34:06 +0200

    fix: migrate functions back to cjs

--- functions/scripts/build.mjs
+++ functions/scripts/build.mjs
@@ -1,31 +1,28 @@
 import path from 'path';
 import { performance } from 'perf_hooks';
-import chokidar from 'chokidar';
 import esbuild from 'esbuild';
 import { NodeResolvePlugin } from '@esbuild-plugins/node-resolve';
-import { typeCheck, entryPoint, outDir, __dirname } from './utils.js';
-
-process.chdir(path.join(__dirname, '..'));
-
-let builder;
+import { typeCheck, entryPoint, outDir, __dirname } from './utils.mjs';
 
 const build = async () => {
+  process.chdir(path.join(__dirname, '..'));
+
   try {
     process.stdout.write('Building functions...\n');
 
     const now = performance.now();
 
-    const builderPromise = builder
-      ? builder.rebuild()
-      : esbuild.build({
+    await Promise.all([
+      esbuild
+        .build({
           entryPoints: [entryPoint],
           platform: 'node',
-          format: 'esm',
+          format: 'cjs', // TODO: set to 'esm' when Firebase Functions support it
           target: 'node14',
-          sourcemap: 'inline',
+          minify: true,
+          sourcemap: true,
           bundle: true,
           outfile: path.join(outDir, 'index.js'),
-          incremental: true,
           plugins: [
             NodeResolvePlugin({
               extensions: ['.ts', '.js'],
@@ -35,15 +32,12 @@ const build = async () => {
                   : resolved,
             }),
           ],
-        });
-
-    const [newBuilder] = await Promise.all([
-      builderPromise.then((builder) => {
-        process.stdout.write(
-          `Built in ${(performance.now() - now).toFixed(2)}ms.\n`,
-        );
-        return builder;
-      }),
+        })
+        .then(() => {
+          process.stdout.write(
+            `Built in ${(performance.now() - now).toFixed(2)}ms.\n`,
+          );
+        }),
       typeCheck().then(() => {
         process.stdout.write(
           `Type checked in ${(performance.now() - now).toFixed(2)}ms.\n`,
@@ -51,17 +45,10 @@ const build = async () => {
       }),
     ]);
 
-    builder = newBuilder;
-
     process.stdout.write('Done.\n');
   } catch (error) {
     process.stderr.write(`Error!\n${error.stack || error}\n`);
   }
 };
 
-const watcher = chokidar.watch('src/**/*.ts', {
-  ignoreInitial: true,
-});
-
-watcher.on('all', build);
 build();
--- functions/scripts/watch.mjs
+++ functions/scripts/watch.mjs
@@ -1,28 +1,31 @@
 import path from 'path';
 import { performance } from 'perf_hooks';
+import chokidar from 'chokidar';
 import esbuild from 'esbuild';
 import { NodeResolvePlugin } from '@esbuild-plugins/node-resolve';
-import { typeCheck, entryPoint, outDir, __dirname } from './utils.js';
+import { typeCheck, entryPoint, outDir, __dirname } from './utils.mjs';
+
+process.chdir(path.join(__dirname, '..'));
+
+let builder;
 
 const build = async () => {
-  process.chdir(path.join(__dirname, '..'));
-
   try {
     process.stdout.write('Building functions...\n');
 
     const now = performance.now();
 
-    await Promise.all([
-      esbuild
-        .build({
+    const builderPromise = builder
+      ? builder.rebuild()
+      : esbuild.build({
           entryPoints: [entryPoint],
           platform: 'node',
-          format: 'esm',
+          format: 'cjs', // TODO: set to 'esm' when Firebase Functions support it
           target: 'node14',
-          minify: true,
-          sourcemap: true,
+          sourcemap: 'inline',
           bundle: true,
           outfile: path.join(outDir, 'index.js'),
+          incremental: true,
           plugins: [
             NodeResolvePlugin({
               extensions: ['.ts', '.js'],
@@ -32,12 +35,15 @@ const build = async () => {
                   : resolved,
             }),
           ],
-        })
-        .then(() => {
-          process.stdout.write(
-            `Built in ${(performance.now() - now).toFixed(2)}ms.\n`,
-          );
-        }),
+        });
+
+    const [newBuilder] = await Promise.all([
+      builderPromise.then((builder) => {
+        process.stdout.write(
+          `Built in ${(performance.now() - now).toFixed(2)}ms.\n`,
+        );
+        return builder;
+      }),
       typeCheck().then(() => {
         process.stdout.write(
           `Type checked in ${(performance.now() - now).toFixed(2)}ms.\n`,
@@ -45,10 +51,17 @@ const build = async () => {
       }),
     ]);
 
+    builder = newBuilder;
+
     process.stdout.write('Done.\n');
   } catch (error) {
     process.stderr.write(`Error!\n${error.stack || error}\n`);
   }
 };
 
+const watcher = chokidar.watch('src/**/*.ts', {
+  ignoreInitial: true,
+});
+
+watcher.on('all', build);
 build();
