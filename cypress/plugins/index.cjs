bcd377b7f5b4e3e698ff9e3550ec730232e6ad0e
Giorgio Garasto <giorgio@garasto.me>
2022-05-31 08:38:30 +0200

    test: make cypress-axe work with yarn 3

--- cypress/plugins/index.cjs
+++ cypress/plugins/index.cjs
@@ -1,1 +1,7 @@
-module.exports = (on, config) => config;
+const axe = require('axe-core/axe.js');
+
+module.exports = (on, config) => {
+  on('task', {
+    getAxeSource: () => axe.source,
+  });
+};
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,11 @@
 import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.overwrite('injectAxe', () => {
+  cy.task<string>('getAxeSource').then((axeSource) =>
+    cy.window({ log: false }).then((window) => {
+      const script = window.document.createElement('script');
+      script.innerHTML = axeSource;
+      window.document.head.appendChild(script);
+    }),
+  );
+});
