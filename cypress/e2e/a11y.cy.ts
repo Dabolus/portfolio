b996f4bf621d341522aabdaa5638a3d5e7618715
Giorgio Garasto <giorgio@garasto.me>
2022-11-04 20:55:56 +0100

    test: remove exception for home page a11y

--- cypress/e2e/a11y.cy.ts
+++ cypress/e2e/a11y.cy.ts
@@ -4,17 +4,7 @@ describe('accessibility', () => {
       it(`is accessible in the ${page} page`, () => {
         cy.visit(`/en/${page === 'home' ? '' : page}`);
         cy.injectAxe();
-        cy.checkA11y(
-          undefined,
-          // TOOD: re-enable empty-heading rule when Typed.js accessibility issues are fixed
-          page === 'home'
-            ? {
-                rules: {
-                  'empty-heading': { enabled: false },
-                },
-              }
-            : undefined,
-        );
+        cy.checkA11y();
         // Take a snapshot to check the visual diff of the page
         cy.percySnapshot(`${page[0].toUpperCase()}${page.slice(1)}`, {
           percyCSS: `
