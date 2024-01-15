2e318ad78004a3e47d4bd9c72d876672b3d92b34
Giorgio Garasto <giorgio@garasto.me>
2024-01-15 08:28:12 +0100

    ci: re-enable sending report to telegram

--- scripts/postdeploy.ts
+++ scripts/postdeploy.ts
@@ -1,6 +1,6 @@
 import purgeCache from './cloudflare.js';
 import audit from './lighthouse.js';
-// import sendDocuments from './telegram.js';
+import sendDocuments from './telegram.js';
 
 const postdeploy = async (): Promise<void> => {
   try {
@@ -8,21 +8,10 @@ const postdeploy = async (): Promise<void> => {
 
     const report = await audit();
 
-    console.log(
-      report
-        .map(
-          ({ title, scores }) =>
-            `${title}\n${scores
-              .map((category) => `${category.title}: ${category.score}`)
-              .join('\n')}`,
-        )
-        .join('\n\n'),
-    );
+    console.log('Sending report to Telegram...');
+    await sendDocuments(report);
 
-    // console.log('Sending report to Telegram...');
-    // await sendDocuments(report);
-
-    // console.log('Report successfully sent to Telegram.');
+    console.log('Report successfully sent to Telegram.');
     process.exit(0);
   } catch (error) {
     console.error('Error while generating Lighthouse report!', error);
