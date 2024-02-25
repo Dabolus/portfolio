92b4c4b7509e482f1ed8943a7295dce759c3b18c
Giorgio Garasto <giorgio@garasto.me>
2024-02-25 09:48:30 +0100

    chore: delete no more used processImage function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,3 @@
 export { redirectToPreferredLocale } from './i18n.js';
-export { processImage } from './image.js';
 export { sendEmail } from './email.js';
 export { retrieveResume } from './resume.js';
